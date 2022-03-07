100baf278b7bf1046e422e829a3e93f5060bb5b2
bheart7 <bheartshrestha7@gmail.com>
2022-03-07 22:32:31 +0545

    added index file, styles and sandbox typescript file

---
+++ sandbox.ts
@@ -0,0 +1,37 @@
+// arrays
+let names = ['luigi', 'mario', 'yoshi'];
+
+names.push('toad');
+// names.push(3);
+// names[0] = 3;
+
+let numbers = [10, 20, 30, 40];
+
+numbers.push(25);
+// numbers.push('shaun');
+// numbers[1] = 'saun';
+
+let mixed = ['kent', 4, 'chun-li', 8, 9, true];
+mixed.push(false);
+mixed.push(4);
+mixed[0] = true;
+mixed.push('hello baby');
+// console.log('mixed', mixed)
+
+// objects
+let ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+
+ninja['okay'] = true;
+console.log(ninja);
+ninja['skills'] = ['fighting', 'sneaking']
+// ninja.age = '30'
+
+ninja = {
+    name: 'yoshi',
+    belt: 'oragne',
+    age: 40,
+}
